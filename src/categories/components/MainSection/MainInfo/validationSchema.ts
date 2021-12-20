4391d3f11303cb8cabde806c2939eda5c1c7f324
SkyGlowFire <SkyGlowFire@gmail.com>
2021-12-20 22:17:35 +0700

    fixed category description validation

--- src/categories/components/MainSection/MainInfo/validationSchema.ts
+++ src/categories/components/MainSection/MainInfo/validationSchema.ts
@@ -8,7 +8,7 @@ export const schema = yup.object().shape({
       then: rule => rule
       .min(15, "Description text must be between 15-700 characters")
       .max(700, "Description text must be between 15-700 characters")
-      .matches(/^([\w .,\/-]*)$/, "Description text can contain only letters, digits, spaces, and -/. symbols"),
+      .matches(/^([\w .,'\/-]*)$/, "Description text can contain only letters, digits, spaces, and -/. symbols"),
     })
   }, [['text', 'text']]).required()
 
